e03ddb87c598076cae2d859476221a4fd372d4f6
Sami Salkosuo <sami.salkosuo@ieee.org>
2017-09-15 09:45:58 +0300

    combined parse.sh and my_parser_wrapper.sh

--- server/my_parser_wrapper.sh
+++ server/my_parser_wrapper.sh
@@ -1,18 +1,8 @@
 #this script is custom parser wrapper to be called from servlet
-#replaces also parse_conll.sh script
+#replaces parse_conll.sh, parse.sh scripts
 
-#tag the input, and fill in lemmas
-# MAX_SEN_LEN and SEN_CHUNK are defined in servlet
-cat $TMPDIR/$INPUT_TEXT_FILE | $PYTHON conv_u_09.py --output=09 | $PYTHON limit_sentence_len.py -N $MAX_SEN_LEN -C $SEN_CHUNK | ./tag.sh > $TMPDIR/input_tagged.conll09
+#accepts input in standard input
+#preprocesses, parses and postprocesses text & conllu
 
-if [[ $? -ne 0 ]]
-then
-    echo "Tagging of the input failed. Please check the error messages above for any help" 1>&2
-    exit 1
-fi
+cat $TMPDIR/$INPUT_TEXT_FILE | $PYTHON conv_u_09.py --output=09 | $PYTHON limit_sentence_len.py -N $MAX_SEN_LEN -C $SEN_CHUNK | ./tag.sh  | curl -H "Content-Type: text/plain" --data-binary @- http://127.0.0.1:9876/annaparser | $PYTHON conllUtil.py --swap HEAD:=PHEAD,DEPREL:=PDEPREL | $PYTHON limit_sentence_len.py --reverse | $PYTHON conv_u_09.py --output=u > $TMPDIR/$OUTPUT_CONLLU_FILE 2> $TMPDIR/$ERROR_FILE
 
-#parse
-cat $TMPDIR/input_tagged.conll09 | ./parse.sh  > $TMPDIR/input_parsed.conll09
-
-#reverse: tag the input, and fill in lemmas
-cat $TMPDIR/input_parsed.conll09 | $PYTHON limit_sentence_len.py --reverse | python conv_u_09.py --output=u > $TMPDIR/$OUTPUT_CONLLU_FILE 2> $TMPDIR/$ERROR_FILE
