897d1a26c169f96701498bc6f286aaeb704cc90c
Sami Salkosuo <sami.salkosuo@ieee.org>
2017-12-22 14:42:40 +0200

    updated

--- parserfarm/start_parser_farm.sh
+++ parserfarm/start_parser_farm.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 #start local finnish dependency parser farm
+#using proxy
 
 #check if docker network exist
 #if yes ask to stop farm
@@ -13,10 +14,11 @@ fi
 #require a number of parsers when executing this script
 if [[ "$1" == "" ]] ; then
 	echo "Usage: $0 <NUMBER_OF_FINNISH_DEPENDENCY_PARSERS> <DOCKER_IMAGE_VERSION>"
-	echo "Default <DOCKER_IMAGE_VERSION> is 'latest'"  
+	echo "Default <DOCKER_IMAGE_VERSION> is 'latest'"    
 	exit 1
 fi  
 __number_of_parsers=$1
+
 if [[ "$2" == "" ]] ; then
 	__findep_image_version=latest
 else
@@ -28,12 +30,10 @@ __prefix=findepparser
 __network_name=${__prefix}network
 __proxy_port=9876
 __findep_parser_image=kazhar/finnish-dep-parser:$__findep_image_version
-#comma separated environment variables for findep parser container
+#colon separated environment variables for findep parser container
 #comment out if not needed
-__findep_env_vars=server_feature=DEP
-#,conllu_cache_size=100
-
-
+__findep_env_vars=server_feature=DEP,FWN
+#:enable_cache=false
 
 if [ ! -f haproxy_template.cfg ]; then
     echo "haproxy_template.cfg not found."
@@ -46,7 +46,7 @@ cp haproxy_template.cfg ${__proxy_cfg_file}
 
 #extract env vars to docker -e options
 if [ ! -z "$__findep_env_vars"  ] ; then
-  __findep_env_var_opts=$(echo ${__findep_env_vars} | awk '{n=split($0,a,",");for (i = 1; i <= n; ++i) printf " -e " a[i] " " }')
+  __findep_env_var_opts=$(echo ${__findep_env_vars} | awk '{n=split($0,a,":");for (i = 1; i <= n; ++i) printf " -e " a[i] " " }')
 fi
 
 function startParser
