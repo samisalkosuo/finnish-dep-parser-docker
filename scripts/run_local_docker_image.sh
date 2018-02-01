7986b2f675d7637c90da683e97900e55bc4098f8
Sami Salkosuo <samisalkosuo@users.noreply.github.com>
2018-02-01 12:24:07 +0200

    typo fix

--- scripts/run_local_docker_image.sh
+++ scripts/run_local_docker_image.sh
@@ -2,5 +2,5 @@
 
 # run local Docker image
 __image_name=findep-parser
-docker run -it --rm -p 9876:9876 -e "server_feature=FWM,DEP" ${__image_name}
+docker run -it --rm -p 9876:9876 -e "server_feature=FWN,DEP" ${__image_name}
 
